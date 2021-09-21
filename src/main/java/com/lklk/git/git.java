606277ed580afd9b35fb01b585ba7cc8704e139c
lk666 <31708@qq.com>
2021-09-21 10:47:52 +0800

    first commit

---
+++ src/main/java/com/lklk/git/git.java
@@ -0,0 +1,13 @@
+package com.lklk.git;
+
+import java.sql.SQLOutput;
+
+/**
+ * @author liuk
+ * @create 2021 -09-21-10:44
+ */
+public class git {
+    public static void main(String[] args) {
+        System.out.println("你好，git");
+    }
+}
